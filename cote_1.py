198ed2db4316fec88b89e7af87013d953b869f84
yeol@naver.com <yeol@naver.com>
2024-11-24 19:08:32 +0900

    python cote

---
+++ cote_1.py
@@ -0,0 +1,9 @@
+def solution(order):
+
+    order = str(order)
+    answer = 0
+    for i in range(len(order)):
+        if order[i] in '369':  
+            answer+=1   
+            
+    return answer
---
+++ test.py
@@ -0,0 +1,6 @@
+data = int(input('정수 입력:'))
+
+datas=[]
+for i in range(0,N) :
+    num=int(input('정수 : ')))
+    datas.append(num)
